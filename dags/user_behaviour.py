2939ab7444de50327f8a76254f523b3264399b65
Vyangesh <joshisonu252@gmail.com>
2024-11-26 09:19:35 +0530

    ADD: Added DAG file user_behaviour.py

---
+++ dags/user_behaviour.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+
+default_args = {
+    "owner" : "airflow",
+    "depends_on_past" : True,
+    "wait_for_downstream" : True,
+    "start_date" : datetime(2010,12,1),
+    "email" : ["airflow@airflow.com"],
+    "email_on_failure" : False,
+    "email_on_retry" : False,
+    "retries" : 1,
+    "retry_delay" : timedelta(minutes=5)
+}
+
+dag = DAG("user_behaviour", default_args=default_args, schedule_interval="0 0 * * *", max_active_runs=1)
+
+end_of_data_pipeline = DummyOperator(task_id='end_of_data_pipeline', dag=dag)
+
+end_of_data_pipeline
