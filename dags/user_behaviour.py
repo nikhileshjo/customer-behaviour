2dd8b8fdb81ab77f4a5c7e512e93163af1b21f39
Vyangesh <joshisonu252@gmail.com>
2024-11-28 08:49:41 +0530

    ADD: added two tasks to airflow DAG user_behaviour.py; user_purchase_to_s3_stage & remove_local_user_purchase_file

--- dags/user_behaviour.py
+++ dags/user_behaviour.py
@@ -3,9 +3,24 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.postgres_operator import PostgresOperator
+from airflow.hooks.S3_hook import S3Hook
+from airflow.operators import PythonOperator
+import os
 
 unload_user_purchase = './scripts/sql/filter_unload_user_purchase.sql'
 temp_filtered_user_purchase = '/temp/temp_filtered_user_purchase.csv'
+BUCKET_NAME = ''
+temp_filtered_user_purchase_key = 'user_purchase/stage/{{ds}}/temp_filtered_user_purchase.csv'
+
+def _local_to_s3(filename, key, bucket_name = BUCKET_NAME) :
+    s3 = S3Hook()
+    s3.local_file(filename = filename, bucket_name = bucket_name, replace = True, key = key)
+
+def remove_local_file(filelocation):
+    if os.path.isfile(filelocation):
+        os.remove(filelocation)
+    else:
+        logging.info(f'File {filelocation} not found')
 
 default_args = {
     "owner" : "airflow",
@@ -33,4 +48,23 @@ pg_unload = PostgresOperator(
     wait_for_downstream = True
 )
 
-pg_unload>>end_of_data_pipeline
+user_purchase_to_s3_stage = PythonOperator(
+    dag = dag,
+    task_id = 'user_purchase_to_s3_stage',
+    python_callable = _local_to_s3,
+    op_kwargs = {
+        'filename' : temp_filtered_user_purchase,
+        'key' : temp_filtered_user_purchase_key
+    }
+)
+
+remove_local_user_purchase_file = PythonOperator(
+    dag = dag,
+    task_id = 'remove_local_user_purchase_file',
+    python_callable = remove_local_file,
+    op_kwargs = {
+        'filelocation' : temp_filtered_user_purchase
+    }
+)
+
+pg_unload>>user_purchase_to_s3_stage>>remove_local_user_purchase_file>>end_of_data_pipeline
