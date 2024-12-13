3501f9c4a49a0ec7858ebaebb10bbea69a6b0eac
Vyangesh <joshisonu252@gmail.com>
2024-12-13 18:21:40 +0530

    FIX: Removed comments that were for testing

--- dags/user_behaviour.py
+++ dags/user_behaviour.py
@@ -51,39 +51,39 @@ default_args = {
 
 dag = DAG("user_behaviour", default_args=default_args, schedule_interval="0 0 * * *", max_active_runs=1)
 
-#end_of_data_pipeline = DummyOperator(task_id='end_of_data_pipeline', dag=dag)
+end_of_data_pipeline = DummyOperator(task_id='end_of_data_pipeline', dag=dag)
 
 #loads postgres data to a local location in the computer filtered by date
-# pg_unload = PostgresOperator(
-#     dag  = dag,
-#     task_id = 'pg_unload',
-#     sql = unload_user_purchase,
-#     postgres_conn_id = 'postgres_default',
-#     params = {'temp_filtered_user_purchase': temp_filtered_user_purchase},
-#     depends_on_past = True,
-#     wait_for_downstream = True
-# )
+pg_unload = PostgresOperator(
+    dag  = dag,
+    task_id = 'pg_unload',
+    sql = unload_user_purchase,
+    postgres_conn_id = 'postgres_default',
+    params = {'temp_filtered_user_purchase': temp_filtered_user_purchase},
+    depends_on_past = True,
+    wait_for_downstream = True
+)
 
 #Uploads the extracted CSV to S3
-# user_purchase_to_s3_stage = PythonOperator(
-#     dag = dag,
-#     task_id = 'user_purchase_to_s3_stage',
-#     python_callable = _local_to_s3,
-#     op_kwargs = {
-#         'filename' : temp_filtered_user_purchase,
-#         'key' : temp_filtered_user_purchase_key
-#     }
-# )
+user_purchase_to_s3_stage = PythonOperator(
+    dag = dag,
+    task_id = 'user_purchase_to_s3_stage',
+    python_callable = _local_to_s3,
+    op_kwargs = {
+        'filename' : temp_filtered_user_purchase,
+        'key' : temp_filtered_user_purchase_key
+    }
+)
 
 #removes the file extracted from PostgreSQL
-# remove_local_user_purchase_file = PythonOperator(
-#     dag = dag,
-#     task_id = 'remove_local_user_purchase_file',
-#     python_callable = remove_local_file,
-#     op_kwargs = {
-#         'filelocation' : temp_filtered_user_purchase
-#     }
-# )
+remove_local_user_purchase_file = PythonOperator(
+    dag = dag,
+    task_id = 'remove_local_user_purchase_file',
+    python_callable = remove_local_file,
+    op_kwargs = {
+        'filelocation' : temp_filtered_user_purchase
+    }
+)
 
 #copy EMR script to S3
 emr_script_to_s3 = PythonOperator(
@@ -138,7 +138,7 @@ movie_review_to_s3_stage = PythonOperator(
 
 
 #PostgreSQL to Redshift
-#pg_unload>>user_purchase_to_s3_stage>>remove_local_user_purchase_file>>end_of_data_pipeline
+pg_unload>>user_purchase_to_s3_stage>>remove_local_user_purchase_file>>end_of_data_pipeline
 
 #CSV dropped by client to Redshift
 [movie_review_to_s3_stage, emr_script_to_s3] >> add_emr_steps >> clean_movie_review_data
